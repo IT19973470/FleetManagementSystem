3cc6be6e631a14f7ecfb48eeb7608fa7a3822e2d
Gayanpoor <71841314+Gayanpoor@users.noreply.github.com>
2021-08-01 23:51:58 +0530

    Gayan entity
    hello

---
+++ Backend/src/main/java/lk/fleet/entity/Passenger.java
@@ -0,0 +1,91 @@
+package lk.fleet.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+public class Passenger {
+    @Id
+    private String EmpID;
+    private String NIC;
+    private  String first_name;
+    private  String last_name;
+    private String Address;
+    private LocalDate DOB;
+    private int contactnumber;
+    private String password;
+    private LocalDate Register_Date;
+
+    public String getEmpID() {
+        return EmpID;
+    }
+
+    public void setEmpID(String empID) {
+        EmpID = empID;
+    }
+
+    public String getNIC() {
+        return NIC;
+    }
+
+    public void setNIC(String NIC) {
+        this.NIC = NIC;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public LocalDate getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(LocalDate DOB) {
+        this.DOB = DOB;
+    }
+
+    public int getContactnumber() {
+        return contactnumber;
+    }
+
+    public void setContactnumber(int contactnumber) {
+        this.contactnumber = contactnumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getRegister_Date() {
+        return Register_Date;
+    }
+
+    public void setRegister_Date(LocalDate register_Date) {
+        Register_Date = register_Date;
+    }
+}
