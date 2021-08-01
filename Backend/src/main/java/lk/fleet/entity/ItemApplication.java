f3ba16d67445e56072fb1ca802596cdb8c6a38ea
Gayanpoor <71841314+Gayanpoor@users.noreply.github.com>
2021-08-01 23:59:47 +0530

    change entity names

--- Backend/src/main/java/lk/fleet/entity/ItemApplication.java
+++ Backend/src/main/java/lk/fleet/entity/ItemApplication.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
-public class Item_Application {
+public class ItemApplication {
     @Id
     public String RequestID;
     public  int No_of_Items_Carrying;
--- Backend/src/main/java/lk/fleet/entity/PassengerApplication.java
+++ Backend/src/main/java/lk/fleet/entity/PassengerApplication.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
-public class Passenger_Application {
+public class PassengerApplication {
     @Id
     public String RequestID;
     public  String No_of_passengers;
