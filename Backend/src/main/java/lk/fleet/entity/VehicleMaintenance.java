df5ecfbe299df371771f42e7edf42f6f7244fb5e
MonaliBandhuratne <it19537016@my.sliit.lk>
2021-08-02 23:35:53 +0530

    Created entities

---
+++ Backend/src/main/java/lk/fleet/entity/VehicleMaintenance.java
@@ -0,0 +1,57 @@
+package lk.fleet.entity;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class VehicleMaintenance {
+
+
+    @Id
+    private String maintenanceID;
+    private String driverName;
+    private String vehicleID;
+    private String vehicleType;
+    private String maintenanceReason;
+
+    public String getMaintenanceID() {
+        return maintenanceID;
+    }
+
+    public void setMaintenanceID(String maintenanceID) {
+        this.maintenanceID = maintenanceID;
+    }
+
+    public String getDriverName() {
+        return driverName;
+    }
+
+    public void setDriverName(String driverName) {
+        this.driverName = driverName;
+    }
+
+    public String getVehicleID() {
+        return vehicleID;
+    }
+
+    public void setVehicleID(String vehicleID) {
+        this.vehicleID = vehicleID;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public String getMaintenanceReason() {
+        return maintenanceReason;
+    }
+
+    public void setMaintenanceReason(String maintenanceReason) {
+        this.maintenanceReason = maintenanceReason;
+    }
+}
