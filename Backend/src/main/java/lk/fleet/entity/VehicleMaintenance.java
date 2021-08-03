aa27075154af2c2a9f9ea50cfdd1fd407dc5b96c
MonaliBandhuratne <it19537016@my.sliit.lk>
2021-08-03 19:22:04 +0530

    entity update

--- Backend/src/main/java/lk/fleet/entity/VehicleMaintenance.java
+++ Backend/src/main/java/lk/fleet/entity/VehicleMaintenance.java
@@ -3,55 +3,51 @@ package lk.fleet.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.time.LocalDate;
 
 @Entity
 public class VehicleMaintenance {
 
-
     @Id
     private String maintenanceID;
-    private String driverName;
-    private String vehicleID;
-    private String vehicleType;
+    private String maintenanceStatus;
     private String maintenanceReason;
+    private LocalDate maintenanceDate;
+
 
     public String getMaintenanceID() {
+
         return maintenanceID;
     }
 
     public void setMaintenanceID(String maintenanceID) {
+
         this.maintenanceID = maintenanceID;
     }
 
-    public String getDriverName() {
-        return driverName;
-    }
-
-    public void setDriverName(String driverName) {
-        this.driverName = driverName;
-    }
-
-    public String getVehicleID() {
-        return vehicleID;
-    }
-
-    public void setVehicleID(String vehicleID) {
-        this.vehicleID = vehicleID;
-    }
-
-    public String getVehicleType() {
-        return vehicleType;
-    }
-
-    public void setVehicleType(String vehicleType) {
-        this.vehicleType = vehicleType;
-    }
-
     public String getMaintenanceReason() {
+
         return maintenanceReason;
     }
 
     public void setMaintenanceReason(String maintenanceReason) {
+
         this.maintenanceReason = maintenanceReason;
+    }
+
+    public String getMaintenanceStatus() {
+        return maintenanceStatus;
+    }
+
+    public void setMaintenanceStatus(String maintenanceStatus) {
+        this.maintenanceStatus = maintenanceStatus;
+    }
+
+    public LocalDate getMaintenanceDate() {
+        return maintenanceDate;
+    }
+
+    public void setMaintenanceDate(LocalDate maintenanceDate) {
+        this.maintenanceDate = maintenanceDate;
     }
 }
