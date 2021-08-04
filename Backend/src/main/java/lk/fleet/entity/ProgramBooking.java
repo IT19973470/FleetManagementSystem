7ec19d5668cd0bc0129189ec5f6931de95a1d5cd
SashiniUdyani <sashiniudyani14@gmail.com>
2021-08-04 22:03:27 +0530

    Create mappings

--- Backend/src/main/java/lk/fleet/entity/ProgramBooking.java
+++ Backend/src/main/java/lk/fleet/entity/ProgramBooking.java
@@ -2,11 +2,17 @@ package lk.fleet.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class ProgramBooking {
     @Id
     private String programBookingId;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private TVProgram tvProgram;
 
     public String getProgramBookingId() {
         return programBookingId;
@@ -14,5 +20,13 @@ public class ProgramBooking {
 
     public void setProgramBookingId(String programBookingId) {
         this.programBookingId = programBookingId;
+    }
+
+    public TVProgram getTvProgram() {
+        return tvProgram;
+    }
+
+    public void setTvProgram(TVProgram tvProgram) {
+        this.tvProgram = tvProgram;
     }
 }
