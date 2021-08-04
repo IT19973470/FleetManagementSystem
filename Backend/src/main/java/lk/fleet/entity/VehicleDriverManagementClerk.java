c260ed3ddf30bbeaddab25ea66e4bccf30f2a016
varnavi430 <71841456+varnavi430@users.noreply.github.com>
2021-08-05 01:16:04 +0530

    updates

--- Backend/src/main/java/lk/fleet/entity/VehicleDriverManagementClerk.java
+++ Backend/src/main/java/lk/fleet/entity/VehicleDriverManagementClerk.java
@@ -2,11 +2,16 @@ package lk.fleet.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
 @Entity
 public class VehicleDriverManagementClerk {
 
     @Id
     private String vehicleDriverManagementId;
+
+    @OneToOne
+    private UserAccount userAccount;
 
     public String getVehicleDriverManagementId() {
         return vehicleDriverManagementId;
