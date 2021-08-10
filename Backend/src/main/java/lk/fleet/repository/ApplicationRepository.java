92f475abfd72c3fdf6f6b83175c0642daffccef8
Gayanpoor <71841314+Gayanpoor@users.noreply.github.com>
2021-08-11 00:43:06 +0530

    1

--- Backend/src/main/java/lk/fleet/repository/ApplicationRepository.java
+++ Backend/src/main/java/lk/fleet/repository/ApplicationRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 //Gayan//
 public interface ApplicationRepository extends JpaRepository<Application,String> {
-//    Application getApplicationById(String requestID);
+
+
 }
---
+++ Backend/src/main/java/lk/fleet/service/ApplicationService.java
@@ -0,0 +1,7 @@
+package lk.fleet.service;
+
+import lk.fleet.entity.Application;
+
+public interface ApplicationService {
+    Application addApplication(Application application);
+}
