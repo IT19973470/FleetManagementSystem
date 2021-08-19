90ab10616dd7df5915376315cd5c496786ef0e36
Imalka Gunawardana <imalkagunawardana1@gmail.com>
2021-08-19 22:10:12 +0530

    delivery

--- Backend/src/main/java/lk/fleet/service/DeliveryService.java
+++ Backend/src/main/java/lk/fleet/service/DeliveryService.java
@@ -1,4 +1,13 @@
 package lk.fleet.service;
 
+import lk.fleet.dto.DeliveryDTO;
+import lk.fleet.entity.Booking;
+import lk.fleet.entity.Delivery;
+
 public interface DeliveryService {
+    DeliveryDTO addDelivery(Delivery delivery);
+
+    DeliveryDTO updateDelivery(String deliveryId, Booking booking);
+
+    boolean deleteDelivery(String deliveryId);
 }
