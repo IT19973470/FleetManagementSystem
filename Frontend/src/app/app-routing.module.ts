488b00fe0fb365726793e6cc713d730913452ab5
MonaliBandhuratne <it19537016@my.sliit.lk>
2021-08-21 12:25:23 +0530

    UI creation

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -4,18 +4,17 @@ import {LoginComponent} from "./login/login.component";
 import {MainComponent} from "./main/main.component";
 import {ItemDeliveryComponent} from "./main/content/transport-manager/item-delivery/item-delivery.component";
 import {ViewItemDeliveryComponent} from "./main/content/transport-manager/view-item-delivery/view-item-delivery.component";
-import {DriverAccountComponent} from "./main/content/Driver/driver-account/driver-account.component";
-import {DriverRegistrationComponent} from "./main/content/Driver/driver-registration/driver-registration.component";
-import {FuelUpdateComponent} from "./main/content/Driver/fuel-update/fuel-update.component";
-import {OverTimeComponent} from "./main/content/Driver/over-time/over-time.component";
-import {ShiftDetailsComponent} from "./main/content/Driver/shift-details/shift-details.component";
+
 import {PassengerDeliveryComponent} from "./main/content/transport-manager/passenger-delivery/passenger-delivery.component";
 import {ViewPassengerDeliveryComponent} from "./main/content/transport-manager/view-passenger-delivery/view-passenger-delivery.component";
-import {UpdateItemDeliveryComponent} from "./main/content/transport-manager/view-item-delivery/update-item-delivery/update-item-delivery.component";
+
 import {CreateUserAccountComponent} from "./main/content/general-manager/create-user-account/create-user-account.component";
 import {AvailableTransportsComponent} from "./main/content/applicant/available-transports/available-transports.component";
 import {CreateNewRequestComponent} from "./main/content/applicant/create-new-request/create-new-request.component";
-import {UpdatePassengerDeliveryComponent} from "./main/content/transport-manager/view-passenger-delivery/update-passenger-delivery/update-passenger-delivery.component";
+import {VehicleAccidentComponent} from "./main/content/accident-maintenance-manager/vehicle-accident/vehicle-accident.component";
+import {VehicleMaintenanceComponent} from "./main/content/accident-maintenance-manager/vehicle-maintenance/vehicle-maintenance.component";
+import {VehicleAccidentReportComponent} from "./main/content/accident-maintenance-manager/vehicle-accident-report/vehicle-accident-report.component";
+import {VehicleMaintenanceReportComponent} from "./main/content/accident-maintenance-manager/vehicle-maintenance-report/vehicle-maintenance-report.component";
 
 
 const routes: Routes = [
@@ -41,34 +40,7 @@ const routes: Routes = [
         component: ViewItemDeliveryComponent
       },
       {
-        path: 'driver_account',
-        component: DriverAccountComponent
-      },
-      {
-        path: 'driver_registration',
-        component: DriverRegistrationComponent
-      },
-      {
-        path: 'fuel_update',
-        component: FuelUpdateComponent
-      },
-      {
-        path: 'over_time',
-        component: OverTimeComponent
-      },
-      {
-        path: 'shift_details',
-        component: ShiftDetailsComponent
-      },
-      {
-        path: 'update_item_delivery',
-        component: UpdateItemDeliveryComponent
-      },
-      {
-        path: 'update_passenger_delivery',
-        component: UpdatePassengerDeliveryComponent
-      },
-      {
+
         path: 'passenger_delivery',
         component: PassengerDeliveryComponent
       },
@@ -87,7 +59,23 @@ const routes: Routes = [
       {
         path: 'create_new_request',
         component: CreateNewRequestComponent
-      }
+      },
+      {
+        path: 'view_accidents',
+        component:VehicleAccidentComponent
+      },
+      {
+        path: 'view_maintenance',
+        component:VehicleMaintenanceComponent
+      },
+      {
+        path: 'accident_report',
+        component: VehicleAccidentReportComponent
+      },
+      {
+        path: 'maintenance_reports',
+        component: VehicleMaintenanceReportComponent
+      },
     ]
   },
 ];
---
+++ Frontend/src/app/main/content/accident-maintenance-manager/vehicle-maintenance-report/vehicle-maintenance-report.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-maintenance-report',
+  templateUrl: './vehicle-maintenance-report.component.html',
+  styleUrls: ['./vehicle-maintenance-report.component.css']
+})
+export class VehicleMaintenanceReportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
