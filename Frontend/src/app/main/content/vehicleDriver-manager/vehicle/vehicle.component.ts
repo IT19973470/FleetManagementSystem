55ff372f9f2a0630e4c3e05b7656cdeb2934c15f
varnavi430 <71841456+varnavi430@users.noreply.github.com>
2021-08-26 22:57:33 +0530

    fr

--- Frontend/src/app/main/content/vehicleDriver-manager/vehicle/vehicle.component.ts
+++ Frontend/src/app/main/content/vehicleDriver-manager/vehicle/vehicle.component.ts
@@ -39,14 +39,59 @@ export class VehicleComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onSubmit() {
-    return false;
+  //   onSubmit() {
+  //   this.vehicleDriverManagerService.addVehicle(this.vehicleDetail).subscribe((vehicleDetail) => {
+  //     this.router.navigate(['/main/vehicle'])
+  //
+  // }
+  OnSubmitVehicle(){
+    console.log(this.vehicle)
+    this.vehicleDetail.vehicleVehicleDetails.push(this.vehicle);
+    this.vehicle=this.getNewVehicle();
+    this.vehicleForm.resetForm(this.vehicle);
+
   }
 
-  setItem(item: any, i: number) {
+  setVehicle(vehicle) {
+    this.vehicle.vehicleId=vehicle.vehicleId;
+    this.vehicle.vehicleType=vehicle.vehicleType;
+    this.vehicle.model=vehicle.model;
+    this.vehicle.noOfSeats=vehicle.noOfSeats;
+    this.vehicle.initialMeter=vehicle.initialMeter;
+    this.vehicle.serviceMeter=vehicle.serviceMeter;
+    this.vehicle.fuelBalance=vehicle.fuelBalance;
+    this.vehicle.fuelType=vehicle.fuelType;
+    this.vehicle.fuelConsumption=vehicle.fuelConsumption;
+    this.vehicle.occupied=vehicle.occupied;
 
+
+  }
+  setNewVehicle() {
+    this.vehicle = this.getNewVehicle();
+    this.vehicleForm.resetForm(this.vehicle);
+    this.btnText = 'Add';
   }
   getNewVehicle() {
+    return{
+      vehicleId:'',
+      vehicleType:'',
+      model:'',
+      noOfSeats:'',
+      initialMeter:'',
+      serviceMeter:'',
+      fuelBalance:'',
+      fuelType:'',
+      fuelConsumption:'',
+      occupied:''
+
+
+
+
+
+
+
+
+    };
 
   }
 }
