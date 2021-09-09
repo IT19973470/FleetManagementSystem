48b8f4533dc8b79cdbc38d97d5e0c06d00b62f47
SashiniUdyani <sashiniudyani14@gmail.com>
2021-09-10 01:38:55 +0530

    make changes

--- Backend/src/main/java/lk/fleet/dto/PassengerPassengerApplicationDTO.java
+++ Backend/src/main/java/lk/fleet/dto/PassengerPassengerApplicationDTO.java
@@ -30,24 +30,7 @@ public class PassengerPassengerApplicationDTO {
     }
 
     public PassengerPassengerApplicationDTO(PassengerApplicationDTO passengerApplicationDTO){
-        this.passengerApplicationID = passengerApplicationDTO.getApplicationID();
         this.passengerId = passengerApplicationDTO.getApplicationID();
-    }
-
-    public String getPassengerApplicationID() {
-        return passengerApplicationID;
-    }
-
-    public void setPassengerApplicationID(String passengerApplicationID) {
-        this.passengerApplicationID = passengerApplicationID;
-    }
-
-    public String getPassengerId() {
-        return passengerId;
-    }
-
-    public void setPassengerId(String passengerId) {
-        this.passengerId = passengerId;
     }
   
     public void setPassengerName(String passengerName) {
