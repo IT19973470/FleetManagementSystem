2dfd9f2c613205ef3878f3299c6ae5bcc69f8bfd
Imalka Gunawardana <imalkagunawardana1@gmail.com>
2021-09-13 14:41:38 +0530

    Create InitDB.java

---
+++ src/main/java/lk/fleet/util/InitDB.java
@@ -0,0 +1,36 @@
+package lk.fleet.util;
+
+import lk.fleet.entity.UserAccount;
+import lk.fleet.repository.UserAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+public class InitDB {
+
+    @Autowired
+    private UserAccountRepository userAccountRepository;
+
+    @EventListener
+    public void appReady(ApplicationReadyEvent event) {
+        if (userAccountRepository.findAll().size() == 0) {
+            UserAccount userAccount = new UserAccount();
+            userAccount.setEmployeeID("GM001");
+            userAccount.setName("Sashini");
+            userAccount.setNameWithInitials("A.B.Sashini");
+            userAccount.setEmail("sashini@gmail.com");
+            userAccount.setAccountType("GM");
+            userAccount.setContactNo("0764105422");
+            userAccount.setDob(LocalDate.parse("1995-08-12"));
+            userAccount.setNic("956742089V");
+            userAccount.setRegisteredDate(LocalDate.now());
+            userAccount.setPassword("admin");
+            userAccountRepository.save(userAccount);
+        }
+    }
+
+}
