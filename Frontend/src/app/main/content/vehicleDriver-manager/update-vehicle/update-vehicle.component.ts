939684f5ccf5d981a245c34e2652c9aa4bb058c2
varnavi430 <71841456+varnavi430@users.noreply.github.com>
2021-09-14 22:44:55 +0530

    ui

--- Frontend/src/app/main/content/vehicleDriver-manager/update-vehicle/update-vehicle.component.ts
+++ Frontend/src/app/main/content/vehicleDriver-manager/update-vehicle/update-vehicle.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {VehicleDriverManagerService} from "../../../../_service/vehicle-driver-manager.service";
 import {Router} from "@angular/router";
+import {NotifierService} from "angular-notifier";
+
 
 @Component({
   selector: 'app-update-vehicle',
@@ -23,8 +25,12 @@ export class UpdateVehicleComponent implements OnInit {
     occupied: '',
     fuelType: ''
   };
+  private alertBox: any;
+  private alertService: any;
+  private notifierService: any;
 
   constructor(private vehicleDriverManagerService: VehicleDriverManagerService, private router: Router) {
+
 
   }
 
@@ -33,10 +39,20 @@ export class UpdateVehicleComponent implements OnInit {
   }
 
   OnSubmitVehicle() {
-    console.log(this.vehicleDetail)
-    this.vehicleDriverManagerService.updateVehicle(this.vehicleDetail).subscribe((vehicle) => {
-      this.router.navigate(['/main/view_vehicles'])
-    })
+    // this.alertBox.alert = true;
+    // this.alertBox.msg = 'Do you want to update this delivery?';
+    // this.alertService.reply.observers = [];
+    // this.alertService.reply.subscribe(reply => {
+    //   if(reply) {
+        console.log(this.vehicleDetail)
+        this.vehicleDriverManagerService.updateVehicle(this.vehicleDetail).subscribe((vehicle) => {
+          // let vehicleDetail;
+          // this.vehicleDetail=vehicleDetail;
+          this.notifierService.notify("success", "Delivery updated successfully");
+          this.router.navigate(['/main/view_vehicles'])
+        })
+
+
   }
   removeVehicle() {
     this.vehicleDriverManagerService.deleteVehicle(this.vehicleDetail.vehicleId).subscribe((reply) => {
