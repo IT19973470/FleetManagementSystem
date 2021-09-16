3cf22c25b5ebd0cd86bc05305e50cca1582ad6d4
Gayanpoor <71841314+IT20011970@users.noreply.github.com>
2021-09-16 13:12:43 +0530

    new

--- Frontend/src/app/main/content/driver/driver-registration/driver-registration.component.ts
+++ Frontend/src/app/main/content/driver/driver-registration/driver-registration.component.ts
@@ -21,6 +21,7 @@ export class DriverRegistrationComponent implements OnInit {
       nic: '',
       dob: '',
       name: '',
+      approved: true,
       address: '',
       contactNo: '',
       email: '',
