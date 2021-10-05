f008ac2134105bd0aefa7a4521a373b6bdf8561c
Imalka Gunawardana <imalkagunawardana1@gmail.com>
2021-10-06 01:32:17 +0530

    errors fixed

--- Frontend/src/app/main/content/vehicleDriver-manager/driver-confirm/driver-confirm.component.ts
+++ Frontend/src/app/main/content/vehicleDriver-manager/driver-confirm/driver-confirm.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {VehicleDriverManagerService} from "../../../../_service/vehicle-driver-manager.service";
-import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-driver-confirm',
@@ -9,68 +7,9 @@ import {Router} from "@angular/router";
 })
 export class DriverConfirmComponent implements OnInit {
 
-  driverDetails = [];
-
-  isModalTable = {
-    text: '',
-    openTable: false,
-    foundItem: ''
-  };
-  user: boolean = true;
-  item: boolean = true;
-  tblIndex;
-
-  driverID;
-  selectedDriver;
-  selectedUserAccount;
-
-
-
-
-
-
-
-
-
-
-  constructor(private vehicleDriverManagerService: VehicleDriverManagerService, private router: Router) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.getDriverRequest();
-  }
-//get all driver requests
-  getDriverRequest() {
-    this.vehicleDriverManagerService.getDriverRequest().subscribe((driverDetails) => {
-      this.driverDetails = driverDetails;
-      // console.log(this.vehicles)
-    })
   }
 
-  approveDriver(approval) {
-    this.vehicleDriverManagerService.approveDriver(this.selectedDriver.driverID, approval).subscribe((driver) => {
-      this.selectedDriver.approval = driver.approval;
-    })
-
-  }
-  // approveUserAccount(approval) {
-  //   this.vehicleDriverManagerService.approveUserAccount(this.selectedUserAccount.employeeID, approval).subscribe((userAccount) => {
-  //     this.selectedUserAccount.approved = userAccount.approved;
-  //   })
-  // }
-
-
-
-  getTransportByID() {
-
-  }
-
-  setDriver(driver) {
-    this.selectedDriver = driver;
-    this.isTrueOrFalse(true);
-
-  }
-
-  isTrueOrFalse(reply) {
-    this.isModalTable.openTable = reply;
-  }
 }
