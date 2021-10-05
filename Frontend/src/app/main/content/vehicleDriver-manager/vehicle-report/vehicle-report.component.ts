c74a7717d45aabc890c3ecc4029032e8ca624e5a
varnavi430 <71841456+varnavi430@users.noreply.github.com>
2021-10-06 01:02:07 +0530

    up

--- Frontend/src/app/main/content/vehicleDriver-manager/vehicle-report/vehicle-report.component.ts
+++ Frontend/src/app/main/content/vehicleDriver-manager/vehicle-report/vehicle-report.component.ts
@@ -1,56 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-vehicle-report',
-  templateUrl: './vehicle-report.component.html',
-  styleUrls: ['./vehicle-report.component.css']
-})
-export class VehicleReportComponent implements OnInit {
-  deliveryItemDetails = [];
-  days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-  weeklyVehicles = [
-    {
-      week: 'Week 1',
-      pd: {
-        deliveries: 100,
-        completed: 50,
-        cancelled: 50
-      },
-      id: {
-        deliveries: 90,
-        completed: 40,
-        cancelled: 50
-      },
-      pid: {
-        deliveries: 80,
-        completed: 50,
-        cancelled: 30
-      }
-    },
-    {
-      week: 'Week 2',
-      pd: {
-        deliveries: 100,
-        completed: 50,
-        cancelled: 50
-      },
-      id: {
-        deliveries: 90,
-        completed: 40,
-        cancelled: 50
-      },
-      pid: {
-        deliveries: 80,
-        completed: 50,
-        cancelled: 30
-      }
-    },
-  ]
-  chartOptions: any;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+// import { Component, OnInit } from '@angular/core';
+//
+// @Component({
+//   selector: 'app-vehicle-report',
+//   templateUrl: './vehicle-report.component.html',
+//   styleUrls: ['./vehicle-report.component.css']
+// })
+// export class VehicleReportComponent implements OnInit {
+//   deliveryItemDetails = [];
+//   days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+//   weeklyVehicles = [
+//     {
+//       week: 'Week 1',
+//       pd: {
+//         deliveries: 100,
+//         completed: 50,
+//         cancelled: 50
+//       },
+//       id: {
+//         deliveries: 90,
+//         completed: 40,
+//         cancelled: 50
+//       },
+//       pid: {
+//         deliveries: 80,
+//         completed: 50,
+//         cancelled: 30
+//       }
+//     },
+//     {
+//       week: 'Week 2',
+//       pd: {
+//         deliveries: 100,
+//         completed: 50,
+//         cancelled: 50
+//       },
+//       id: {
+//         deliveries: 90,
+//         completed: 40,
+//         cancelled: 50
+//       },
+//       pid: {
+//         deliveries: 80,
+//         completed: 50,
+//         cancelled: 30
+//       }
+//     },
+//   ]
+//   chartOptions: any;
+//
+//   constructor() { }
+//
+//   ngOnInit(): void {
+//   }
+//
+// }
